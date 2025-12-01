c789cfc54b5463f79c620b575806ab2d964af4d6
Samarth <samarthkalshetti0@gmail.com>
2025-12-02 00:53:42 +0530

    fix: updated database url

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  await mongoose.connect(process.env.DATABASE_URL);
 }
 
 // ---------- VIEWS ----------
